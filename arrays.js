6066dd71a3e774b2cbe465939a16977b20cd8e3b
Anthony Turetsky <tturetsky@gmail.com>
2018-05-01 22:39:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,7 @@ function accessElementInArray(array,index) {
 return array[index];
 }
 
+<<<<<<< HEAD
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
@@ -41,3 +42,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
  return array.slice(0, array.length - 1);
 }
+
+
+
